9bfcabab6c68fa6c530de7bd9ad1b8e39ecd7b92
saprykinarteom <saprykinarteom@gmail.com>
2020-06-15 14:03:10 +0300

    creet

---
+++ src/main/java/ru/saprykinav/familyhub/FamilyhubApplication.java
@@ -0,0 +1,13 @@
+package ru.saprykinav.familyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/saprykinav/familyhub/FamilyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saprykinav.familyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
