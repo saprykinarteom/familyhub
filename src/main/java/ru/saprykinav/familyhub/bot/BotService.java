449684b09c266cfd4d75fd5c66fa16a117302b06
saprykinarteom <saprykinarteom@gmail.com>
2020-10-09 21:36:55 +0300

    fixed bussines logic mistake now

--- src/main/java/ru/saprykinav/familyhub/bot/BotService.java
+++ src/main/java/ru/saprykinav/familyhub/bot/BotService.java
@@ -46,16 +46,12 @@ public class BotService {
             BigDecimal sumBuysByMemberOne = botBuyService.getSumBuysByCustomerAfterLastPayDay(familyMemberOne);
             BigDecimal sumBuysByMemberTwo = botBuyService.getSumBuysByCustomerAfterLastPayDay(familyMemberTwo);
 
-            BigDecimal temp1 = sumBuysByMemberOne.subtract(sumBuysByMemberTwo);
-            BigDecimal temp2 = sumBuysByMemberTwo.subtract(sumBuysByMemberOne);
+            BigDecimal temp1 = sumBuysByMemberOne.subtract(sumBuysByMemberTwo).divide(BigDecimal.valueOf(2));
+            BigDecimal temp2 = sumBuysByMemberTwo.subtract(sumBuysByMemberOne).divide(BigDecimal.valueOf(2));
             BigDecimal temp3 = customer.getFamily().getMandatorySpending().divide(BigDecimal.valueOf(2));
 
-            BigDecimal debtMemberOneTemp = temp3.subtract(temp1);
-            BigDecimal debtMemberTwoTemp = temp3.subtract(temp2);
-
-            BigDecimal debtMemberOne = debtMemberOneTemp.divide(BigDecimal.valueOf(2));
-            BigDecimal debtMemberTwo = debtMemberTwoTemp.divide(BigDecimal.valueOf(2));
-
+            BigDecimal debtMemberOne = temp3.subtract(temp1);
+            BigDecimal debtMemberTwo = temp3.subtract(temp2);
 
             String text = "Нужно заплатить " + customer.getFamily().getMandatorySpending()
                     + "\nпокупки за месяц : \n"
